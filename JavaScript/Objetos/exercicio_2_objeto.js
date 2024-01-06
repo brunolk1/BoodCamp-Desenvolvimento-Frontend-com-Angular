1c6527fabf9460982315dfd8a2d7b01bc2e3b391
Bruno Rodrigues <brunorodriguesilva2010@hotmail.com>
2024-01-06 12:48:14 -0300

    ain

--- JavaScript/Objetos/exercicio_2_objeto.js
+++ JavaScript/Objetos/exercicio_2_objeto.js
@@ -3,7 +3,14 @@
 Para cada pessoa teremos os atributos nome, peso, altura.
 As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
 Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor do seu IMC;
-*/
+
+IMC em adulto Condição:
+- Abaixo de 18.5 Abaixo do peso;
+- Entre 18.5 e 25 peso normal;
+- Entre 25 e 30 Acima do peso;
+- Entre 30 e 40 obeso;
+- Acima de 40 obesidade Grave;
+*/ 
 class pessoa {
     nome;
     peso;
@@ -15,14 +22,30 @@ class pessoa {
         this.altura = altura;
     }
 
-    calcularIMC (peso, altura) {
-        return peso / (altura * altura);
+    calcularIMC () {
+        return this.peso / (this.altura * this.altura);
+    }
+
+    classificarIMC () {
+        const imc = this.calcularIMC();
+
+        if (imc < 18.5) {
+            return ('Abaixo do Peso');
+        } else if (imc >= 18.5 && imc < 25) {
+            return ('Peso Normal');
+        } else if (imc >= 25 && imc < 30 ) {
+            return ('Acima do Peso');
+        } else if (imc >= 30 && imc < 40) {
+            return ('Obeso');
+        } else {
+            return ('Obesidade Grave')
+        }
     }
 
 }
 
-const jose = new pessoa ('Jose', '70', '1.75');
-console.log (jose.calcularIMC (70, 1.75));
+const jose = new pessoa ('Jose', 70, 1.75);
+console.log (jose.classificarIMC ());
 
-const bruno = new pessoa ('Bruno', '130', '1,75')
-console.log (bruno.calcularIMC (130, 1.75));
+const bruno = new pessoa ('Bruno', 130, 1.75)
+console.log (bruno.classificarIMC ());
